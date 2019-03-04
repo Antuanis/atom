b619627ac5b6355d687f9ba30e620af176eb6529
antuanis <k1dtosha@gmail.com>
2019-03-04 14:57:14 +0300

    Добавили checkstyle

--- homeworks/HW2/src/main/java/ru/example/Game.java
+++ homeworks/HW2/src/main/java/ru/example/Game.java
@@ -2,8 +2,7 @@ package ru.example;
 
 public class Game {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.print("Hallow World!");
 
     }
